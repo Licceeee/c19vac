3724d174a2b10e3118bafaf3f559c6512723e860
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-03-04 15:59:46 +0100

    detail componend data updated to new fetch

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -27,7 +27,7 @@ const Details = ({ loading, setLoading }) => {
         getApiData(`http://localhost:9000/api/v1/vaccine_types/${id}`)
         .then(result => {
           setLoading(false);
-          setVaccine(result);
+          setVaccine(result[0]);
         })
       }, [id]);
 
@@ -47,51 +47,55 @@ const Details = ({ loading, setLoading }) => {
             vaccine 
             ? 
 
-                <p>YAAAAYYY</p>
-                // <div className={classes.boxClass}>
-                //     <Box width="70%" mx="auto" fontSize='2rem'><h1>{vaccine.fields.name}</h1></Box>
-                //     <Box width="70%" mx="auto" marginBottom="20px"><p>{vaccine.fields.description}</p></Box>
+                
+                <div className={classes.boxClass}>
+                    <Box width="70%" mx="auto" fontSize='2rem'><h1>{vaccine.name}</h1></Box>
+                    <Box width="70%" mx="auto" marginBottom="20px"><p>{vaccine.description}</p></Box>
 
-                //     <Box width="70%" mx="auto" marginBottom="20px"><h2>Benefits</h2></Box>
-                //     <Box width="70%" mx="auto" ><p>{vaccine.fields.benefits}</p></Box>
-
-
-                //     <Box width="70%" mx="auto" marginBottom="20px"><h2>Challenges</h2></Box>
-                //     <Box width="70%" mx="auto" ><p>{vaccine.fields.challenges}</p></Box>
-                //     <br/>
+                    <Box width="70%" mx="auto" marginBottom="20px"><h2>Benefits</h2></Box>
+                    <Box width="70%" mx="auto" ><p>{vaccine.benefits}</p></Box>
 
 
-                //     <Box width="70%" mx="auto">
-                //         <TableContainer component={Paper}>
-                //             <Table className={classes.table} aria-label="simple table">
-                //                 <TableHead>
-                //                     <TableRow >
-                //                         <TableCell className={classes.tableBG}>Vaccines</TableCell>
-                //                         <TableCell className={classes.tableBG}># doses required</TableCell>
-                //                         <TableCell className={classes.tableBG}>Other uses</TableCell>
-                //                     </TableRow>
-                //                 </TableHead>
-                //                 <TableBody>
-                //                     <TableRow >
-                //                         <TableCell>{vaccine.fields.vaccines 
-                //                                                         ? vaccine.fields.vaccines.map(
-                //                                                             (vaccine, index) => {
-                //                                                                 return <p key={index}>{vaccine.fields.name}</p>})
-                //                                                         : "/" }
-                //                         </TableCell>
-                //                         <TableCell>{vaccine.fields.numberOfDosesRequired}</TableCell>
-                //                         <TableCell>{vaccine.fields.otherUsesOfThisTechnology 
-                //                                                         ?  vaccine.fields.otherUsesOfThisTechnology.map(
-                //                                                             (disease, index) => {
-                //                                                                 return <p key={index}>{disease}</p>}) 
-                //                                                         : "/" }
-                //                         </TableCell>
-                //                     </TableRow>
-                //                 </TableBody>
-                //             </Table>
-                //         </TableContainer>
-                //     </Box>
-                // </div>
+                    <Box width="70%" mx="auto" marginBottom="20px"><h2>Challenges</h2></Box>
+                    <Box width="70%" mx="auto" ><p>{vaccine.challenges}</p></Box>
+                    <br/>
+
+
+                    <Box width="70%" mx="auto">
+                        <TableContainer component={Paper}>
+                            <Table className={classes.table} aria-label="simple table">
+                                <TableHead>
+                                    <TableRow >
+                                        <TableCell className={classes.tableBG}>Vaccines</TableCell>
+                                        <TableCell className={classes.tableBG}># doses required</TableCell>
+                                        <TableCell className={classes.tableBG}>Other uses</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow >
+                                        <TableCell>
+                                                    {/* {vaccine.fields.vaccines 
+                                                        ? vaccine.fields.vaccines.map(
+                                                            (vaccine, index) => {
+                                                                return <p key={index}>{vaccine.fields.name}</p>})
+                                                        : "/" 
+                                                    } */}
+                                        </TableCell>
+                                        <TableCell>{vaccine.numberofdosesrequired}</TableCell>
+                                        <TableCell>
+                                                    {/* {vaccine.fields.otherUsesOfThisTechnology 
+                                                        ?  vaccine.fields.otherUsesOfThisTechnology.map(
+                                                            (disease, index) => {
+                                                                return <p key={index}>{disease}</p>}) 
+                                                        : "/" 
+                                                    } */}
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Box>
+                </div>
             : 
                 <p>No data available</p>
                 
