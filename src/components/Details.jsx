e581d25cb3cb1376aa60cd79bb9f0248d12ef4cb
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-03-08 13:40:26 +0100

    add: slash to getapi

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -27,7 +27,7 @@ const Details = ({ loading, setLoading }) => {
 
     useEffect(() => {
         setLoading(true);
-        getApiData(`https://obscure-wildwood-82348.herokuapp.com/api/v1/vaccine_types${id}`)
+        getApiData(`https://obscure-wildwood-82348.herokuapp.com/api/v1/vaccine_types/${id}`)
         .then(result => {
           setLoading(false);
           setVaccine(result.vaccineType);
